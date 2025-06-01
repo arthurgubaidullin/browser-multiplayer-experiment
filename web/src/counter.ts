99701d93117803ca2c766bbec1807a23ff4b1e3f
Artur Gubaidullin <lapteuh@gmail.com>
2025-06-01 17:50:06 +0000

    Update class

--- web/src/counter.ts
+++ web/src/counter.ts
@@ -1,4 +1,3 @@
-import "./app.css";
 import * as Automerge from "@automerge/automerge/next";
 import { computed, signal, Signal, type ReadonlySignal } from "@preact/signals";
 
@@ -7,23 +6,73 @@ type CounterDocument = {
 };
 
 export class Counter {
-  #state: Signal<Automerge.Doc<CounterDocument>>;
+  #state: Signal<Automerge.Doc<CounterDocument> | null>;
+  #socket: WebSocket;
 
   constructor() {
-    const doc = Automerge.from({
-      counter: new Automerge.Counter(),
+    const state = signal<Automerge.Doc<CounterDocument> | null>(null);
+
+    this.#state = state;
+
+    this.#socket = new WebSocket("ws://localhost:3000/ws");
+
+    this.#socket.addEventListener("open", function () {
+      console.debug("Socket connected!");
     });
 
-    this.#state = signal(doc);
+    this.#socket.addEventListener("close", function () {
+      console.debug("Socket disconnected!");
+    });
+
+    this.#socket.addEventListener(
+      "message",
+      async function (event: MessageEvent<Blob>) {
+        console.debug("Message received via web socket.");
+
+        const otherValue = Automerge.load<CounterDocument>(
+          new Uint8Array(await event.data.arrayBuffer())
+        );
+
+        const stateValue = state.peek();
+
+        if (stateValue === null) {
+          state.value = otherValue;
+          return;
+        }
+
+        state.value = Automerge.merge(stateValue, otherValue);
+      }
+    );
   }
 
-  get value(): ReadonlySignal<number> {
-    return computed(() => this.#state.value.counter.value);
+  get value(): ReadonlySignal<number | null> {
+    return computed(() => this.#state.value?.counter.value ?? null);
   }
 
   increment(this: this) {
-    this.#state.value = Automerge.change(this.#state.peek(), (doc) => {
+    const stateValue = this.#state.peek();
+
+    if (stateValue == null) {
+      return;
+    }
+
+    this.#state.value = Automerge.change(stateValue, (doc) => {
       doc.counter.increment(1);
     });
+
+    this.send();
+  }
+
+  private send() {
+    const stateValue = this.#state.peek();
+
+    if (stateValue === null) {
+      return;
+    }
+
+    const bytes = Automerge.save(stateValue);
+    this.#socket.send(bytes);
+
+    console.debug("Message sent via web socket.");
   }
 }
